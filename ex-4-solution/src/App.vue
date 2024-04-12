6dd4cdf44a17e8f6cd55331e9c0eb85927a636f2
Per-Olof Bengtsson <po@niffic.se>
2024-04-12 06:57:05 +0200

    converted to composition API

--- ex-4-solution/src/App.vue
+++ ex-4-solution/src/App.vue
@@ -7,23 +7,13 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { useTodosStore } from './stores/todos';
 import { computed } from 'vue';
 import AddTodo from './components/AddTodo.vue';
 import TodoList from './components/TodoList.vue';
 
-export default {
-  name: 'App',
-  components: {
-    AddTodo,
-    TodoList
-  },
-  setup() {
-    const store = useTodosStore();
-    const completedTodosCount = computed(() => store.completedTodosCount());
+const store = useTodosStore();
+const completedTodosCount = computed(() => store.completedTodosCount());
 
-    return { completedTodosCount };
-  }
-}
 </script>
